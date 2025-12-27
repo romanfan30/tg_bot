a2052baab22e2843e85a6804d218155f2b33b892
romanfan30 <roma.f.2006.rf@gmail.com>
2025-12-27 14:23:42 +0300

    echo bot

---
+++ demo/src/main/java/com/example/demo/TelegramBotConfig.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.context.annotation.Configuration;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+@Configuration
+public class TelegramBotConfig {
+    private final TelegramBot telegramBot;
+
+    public TelegramBotConfig(TelegramBot telegramBot) {
+        this.telegramBot = telegramBot;
+    }
+
+    @PostConstruct
+    public void registerBot() throws TelegramApiException {
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+        telegramBotsApi.registerBot(telegramBot);
+        System.out.println("Bot registered");
+    }
+}
